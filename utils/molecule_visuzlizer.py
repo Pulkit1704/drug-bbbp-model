e1938b1d1356421e6bf49683fb3ad972301f754c
Pulkit <pgoel3379@gmail.com>
2026-07-02 20:32:30 +0530

    add gnn explanation for trained model

--- utils/molecule_visuzlizer.py
+++ utils/molecule_visuzlizer.py
@@ -1,5 +1,8 @@
-import networkx as nx
-import matplotlib.pyplot as plt 
+from rdkit import Chem 
+from rdkit.Chem.Draw import rdMolDraw2D
+from rdkit.Chem import AllChem
+import py3Dmol
+
 
 color_assignment = {
   'Carbon': 'grey',
@@ -11,27 +14,96 @@ color_assignment = {
 }
 
 
-def visualize_networkx(data): 
+def visualize_molecule(mol, 
+                        highlight_atoms = None, 
+                        highlight_colors = None, 
+                        width=500, height=500):
 
-  figure = plt.figure(figsize = (10, 10)) 
+    mol_copy = Chem.Mol(mol) 
 
-  G = nx.Graph() 
+    Chem.Draw.rdMolDraw2D.PrepareMolForDrawing(mol_copy)
 
-  num_nodes = data.x.shape[-1] 
+    drawer = rdMolDraw2D.MolDraw2DSVG(width, height)
+    drawer.drawOptions().useBWAtomPalette() 
 
-  G.add_nodes_from(range(num_nodes))
+    drawer.DrawMolecule(
+        mol,
+        highlightAtoms = highlight_atoms, 
+        highlightAtomColors = highlight_colors
+    )
 
-  edges = data.edge_index.t().tolist() 
+    drawer.FinishDrawing() 
 
-  G.add_edges_from(edges) 
-
-  # node_colors = [color_assignment.get(data.node_labels[i], 'black') for i in range(num_nodes)]
+    return drawer.GetDrawingText() 
 
 
-  positions = nx.kamada_kawai_layout(G) 
+def visualize_2d_quantitative_svg(smiles, 
+                                  node_scores,
+                                  width = 500,
+                                  height = 500):
+    mol = Chem.MolFromSmiles(smiles)
+    if not mol:
+        return "Invalid SMILES"
+    
+    # Chem.Draw.rdMolDraw2D.PrepareMolForDrawing(mol)
+    
+    # Normalize scores to [0, 1]
+    scores = node_scores.detach().cpu().numpy()
+    s_min, s_max = scores.min(), scores.max()
+    
+    # Prevent division by zero if all scores are identical
+    if s_max - s_min > 1e-8:
+        norm_scores = (scores - s_min) / (s_max - s_min)
+    else:
+        norm_scores = scores * 0.0 
 
-  nx.draw(G, pos = positions, 
-          with_labels = True, 
-          labels = data.node_labels) 
-  
-  return figure 
+    atom_colors = {}
+    highlight_atoms = []
+
+    for i in range(mol.GetNumAtoms()):
+        s = norm_scores[i]
+        color = (0.6 - (float(s) * 0.2), 0.53, min(1, 0.45 + (float(s) * 0.5)))
+        
+        atom_colors[i] = color
+        highlight_atoms.append(i)
+
+    drawer_txt = visualize_molecule(mol, 
+                                    highlight_atoms, 
+                                    atom_colors,
+                                    width,
+                                    height)
+
+    return drawer_txt
+
+
+def get_3d_mol(smiles):
+    mol = Chem.MolFromSmiles(smiles)
+
+    AllChem.EmbedMolecule(mol, AllChem.ETKDG())
+    AllChem.UFFOptimizeMolecule(mol)
+
+    return mol
+
+
+def visualize_3d(smiles, node_scores):
+    mol = get_3d_mol(smiles)
+
+    mb = Chem.MolToMolBlock(mol)
+
+    view = py3Dmol.view(width=500, height=500)
+    view.setBackgroundColor('black')
+    view.addModel(mb, "mol")
+    view.setStyle({'stick': {}})
+
+    scores = node_scores.detach().cpu().numpy()
+    scores = (scores - scores.min()) / (scores.max() - scores.min() + 1e-8)
+
+    for i, score in enumerate(scores):
+        if score > 0.6:
+            view.addStyle(
+                {'serial': i + 1},
+                {'sphere': {'color': 'red', 'radius': 0.5}}
+            )
+
+    view.zoomTo()
+    return view
