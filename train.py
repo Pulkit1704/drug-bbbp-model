255fe094b7a598b1b4ef77b4d2042189ae4f24fe
Pulkit <pgoel3379@gmail.com>
2026-07-05 19:36:12 +0530

    create new training script and retrain model

---
+++ train.py
@@ -0,0 +1,48 @@
+from pipeline.graph_pipeline import get_train_test_loaders
+from pipeline.train_pipeline import TrainClassifier
+from utils.io import save_model
+from utils.train_utils import get_pos_wieghts
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+if __name__ == '__main__': 
+
+    train_loader, validation_loader = get_train_test_loaders(batch_size = 1024, train_frac= 0.8)
+
+    model = TrainClassifier(train_loader,
+                            validation_loader,
+                            model_hidden_dim=128,
+                            learning_rate= 0.0001,
+                            pos_weight=get_pos_wieghts(),
+                            model_dropout= 0.5)
+
+    print("Training model...")
+    # implement early stopping:
+    train_loss, validation_loss = model.train_model(epochs = 75)
+
+    print("Model training complete, preparing classification report...")
+    print(model.score_model())
+
+    save_dir = "./trained_model"
+
+    model_checkpoint = {
+        "hyperparameters": model.get_model_hyperparameters(), 
+        "weights": model.classifier.state_dict() 
+    }
+
+    # model.save_model(save_dir)
+    save_model(model_checkpoint, save_dir)
+
+    print(f"model saved to: {save_dir}")
+
+    print("saving loss plot")
+
+    plt.plot(range(len(train_loss)), train_loss, label = 'train loss')
+    plt.plot(range(len(validation_loss)), validation_loss, label = 'validation loss')
+    plt.legend()
+    plt.grid(True)
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss values")
+    plt.savefig(Path(save_dir, "training_plot.png"), dpi = 300)
+
+    print(f"plot saved to: {save_dir}")
